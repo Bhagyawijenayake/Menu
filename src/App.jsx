f5a0e3645c78e141b4432c136140895630aa99ac
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-21 16:44:42 +0530

    categories added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from './data'
 import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategories = menu.map((item) => item.category)
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all',...tempSet]
+// console.log(tempItems);
+
+const allCategories = ['all',...new Set(menu.map((item) => item.category))]
 
 const App = () => {
-const[menuItems,setMenuItems]= useState(menu)
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
   return <main>
     <section className="menu">
       <Title text="our menu" />
+      <Categories categories={categories}/>
       <Menu items={menuItems} /> 
+     
 </section>
   </main>;
 };
